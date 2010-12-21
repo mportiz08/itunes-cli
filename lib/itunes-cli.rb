1faa9ded7d994b5485d41d3d7b3d6b89389ef7e9
Marcus Ortiz <mportiz08@gmail.com>
2010-12-21 12:18:56 -0800

    added gem stuff

---
+++ lib/itunes-cli.rb
@@ -0,0 +1,7 @@
+module ItunesCLI
+  VERSION = "0.0.1"
+end
+
+require 'itunes-cli/track'
+require 'itunes-cli/player'
+require 'itunes-cli/app'
--- lib/itunes_cli.rb
+++
@@ -1,7 +0,0 @@
-module ItunesCLI
-  VERSION = "0.0.1"
-end
-
-require 'itunes_cli/track'
-require 'itunes_cli/player'
-require 'itunes_cli/app'
